2ee01e664326712a8092d0038e63babcffadf14c
lai2-24 <lai2-24@itismeucci.com>
2024-09-25 12:08:41 +0200

    prove thread

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,18 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int l = 200;
+
+        MioThread t1 = new MioThread(l);
+        new Thread(t1).start();
+
+        //System.out.println("Hello world!");
+
+        MioThread t2 = new MioThread(l);
+        new Thread(t2).start();
+        
+        
+
     }
 }
